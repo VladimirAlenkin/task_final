87b9e7925c47db819ca4b6e89662668ad69ea3b7
Vladimir <valenkin@yandex.ru>
2023-02-25 23:50:38 +0500

    Первая часть программы - запрашиваем количество элементов массива, запрашиваем ввод элементов с клавиатуры, считаем количество элементов, удовлетворяющих условию

--- task_final/task_final.cs
+++ task_final/task_final.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine();
+Console.WriteLine("Введите количество элементов массива");
+int n = int.Parse(Console.ReadLine()!);
+string[] str1 = new string[n];
+int count = 0;
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите " + (i +1) + " элемент массива");
+    str1[i] = Console.ReadLine()!;
+    if (str1[i].Length <= 3)
+    {
+        count++;
+    }
+}
